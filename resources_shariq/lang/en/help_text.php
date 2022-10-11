57046d318d6e7eaf9d4b8416ff6ccf9df45bb4ea
Muhammad Aslam <115465415+maslam12@users.noreply.github.com>
2022-10-11 08:42:35 -0700

    d6ne

--- resources_shariq/lang/en/help_text.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'projectName' => 'Enter the title of the project',
-    'caseName' => 'Enter the title of the Case',
-    'projectCategorySettings' => 'Select the category of the project.',
-    'caseCategorySettings' => 'Select the category of the case.',
-    'startDate' => 'Select the start date of project.',
-    'startDateCase' => 'Select the start date of case.',
-];
--- resources_shariq/views/vendor/mail/html/header.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td class="header" style="background-color: {{ $headerColor }}">
-        <a href="{{ $url }}">
-            {{ $slot }}
-        </a>
-    </td>
-</tr>
